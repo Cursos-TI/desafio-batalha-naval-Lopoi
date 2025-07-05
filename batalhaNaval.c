3c1c103dd91916eed4fdd2da355a8cf35eff2166
Lopoi <42871409+Lopoi@users.noreply.github.com>
2025-07-05 15:44:20 +0000

    Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,12 +4,77 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+int main()
+{
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    // Dados do tabuleiro
+    int altura_tabuleiro = 10;
+    int largura_tabuleiro = 10;
+    int tabuleiro[largura_tabuleiro][altura_tabuleiro];
+    // tamanhos dos navios
+    int navio_horizontal_tamanho = 3;
+    int navio_vertical_tamanho = 4;
+    // Posição inicial dos navios (x, y) (iniciando de 0)
+    int navio_horizontal_posicao[2] = {1, 2};
+    int navio_vertical_posicao[2] = {4, 5};
 
+    // Validação da posição dos navios
+    if (navio_vertical_posicao[0] >= largura_tabuleiro || navio_vertical_posicao[1] >= altura_tabuleiro ||
+        navio_vertical_posicao[1] + navio_vertical_tamanho >= altura_tabuleiro ||
+        navio_horizontal_posicao[0] >= largura_tabuleiro || navio_horizontal_posicao[1] >= altura_tabuleiro ||
+        navio_horizontal_posicao[0] + navio_horizontal_tamanho >= largura_tabuleiro)
+    {
+        printf("Navios em posições inválidas\n");
+        return 0;
+    }
+
+    // Montar tabuleiro
+    for (int y = 0; y < altura_tabuleiro; y++)
+    {
+        for (int x = 0; x < largura_tabuleiro; x++)
+        {
+            int sobreposicao = 0;
+            if (x >= navio_horizontal_posicao[0] && x < navio_horizontal_posicao[0] + navio_horizontal_tamanho && y == navio_horizontal_posicao[1])
+            {
+                tabuleiro[x][y] = 3;
+                sobreposicao++;
+            }
+            if (y >= navio_vertical_posicao[1] && y < navio_vertical_posicao[1] + navio_vertical_tamanho && x == navio_vertical_posicao[0])
+            {
+                tabuleiro[x][y] = 3;
+                sobreposicao++;
+            }
+            // validação de sobreposição
+            if(sobreposicao == 2){
+                printf("Navios se sobrepõe\n");
+                return 0;
+            }
+            if(sobreposicao == 0)
+            {
+
+                tabuleiro[x][y] = 0;
+            }
+        }
+    }
+
+    // Mostrar tabuleiro
+    for (int y = 0; y < altura_tabuleiro; y++)
+    {
+        for (int x = 0; x < largura_tabuleiro; x++)
+        {
+            if (x == largura_tabuleiro - 1)
+            {
+                printf(" %d\n", tabuleiro[x][y]);
+            }
+            else
+            {
+                printf(" %d |", tabuleiro[x][y]);
+            }
+        }
+    }
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
@@ -25,7 +90,7 @@ int main() {
     // 0 0 1 0 0
     // 0 1 1 1 0
     // 1 1 1 1 1
-    
+
     // Exemplo para habilidade em octaedro:
     // 0 0 1 0 0
     // 0 1 1 1 0
